aa05ab2b1f85312f3f8b87e0e24f8c251bea219c
BhaveshVA <BhaveshVA>
2025-05-15 11:57:47 +0000

    Added a check-point table
    Added check-point table to save the latest date of the pipeline run. Added the query script to this table, with the number of records and the output file path

--- Pysprak_Notebooks/DMDC Data Pulls Wed and Friday V2.py
+++ Pysprak_Notebooks/DMDC Data Pulls Wed and Friday V2.py
@@ -24,7 +24,8 @@ else:
 # MAGIC CREATE TABLE IF NOT EXISTS dmdc_checkpoint (
 # MAGIC   checkpoint_timestamp TIMESTAMP,
 # MAGIC   filename STRING,
-# MAGIC   record_count INTEGER
+# MAGIC   record_count INTEGER,
+# MAGIC   generate_query STRING
 # MAGIC )
 
 # COMMAND ----------
@@ -39,36 +40,11 @@ scd_data.createOrReplaceTempView('scd_data')
 
 # COMMAND ----------
 
-# MAGIC %md
-# MAGIC ##### Compare below query with the query on cell 8
-
-# COMMAND ----------
-
-# DBTITLE 1,If it is a Friday 1/10 the query should look like this
-# MAGIC %sql
-# MAGIC select count(*) as cg_count
-# MAGIC         from cg_data
-# MAGIC         where SDP_Event_Created_Timestamp >= DATE('2025-01-21')
-# MAGIC         and SDP_Event_Created_Timestamp <= DATE('2025-01-24 00:00:00')
-# MAGIC
-# MAGIC         and Applicant_Type = 'Primary Caregiver'
-# MAGIC         and (Status_Termination_Date is NULL OR Status_Termination_Date >= curdate()
-# MAGIC             OR Status IN ("Approved", "Pending Revocation/Discharge"))
-# MAGIC         and EDIPI is not null
-# MAGIC         
-# MAGIC         union all
-# MAGIC         
-# MAGIC         select count(*) as SCD_count
-# MAGIC         from scd_data
-# MAGIC         where SDP_Event_Created_Timestamp >= DATE('2025-01-21')
-# MAGIC         and SDP_Event_Created_Timestamp <= DATE('2025-01-24 00:00:00')
-# MAGIC         and edipi is not null
-
-# COMMAND ----------
-
 # DBTITLE 1,This function will pull data based on the last run date
 def get_data():
-    last_run_date = (spark.sql("SELECT COALESCE(DATE(max(checkpoint_timestamp)), current_date()) AS last_run_date FROM dmdc_checkpoint").first()[0])
+    last_run_date = spark.sql(
+        'SELECT COALESCE(DATE(max(checkpoint_timestamp)), current_date()) AS last_run_date FROM dmdc_checkpoint'
+    ).first()[0]
 
     now = datetime.now()
     if todays_date.weekday() == 2:
@@ -77,33 +53,32 @@ def get_data():
         today_start_time = datetime(now.year, now.month, now.day)
 
     cg_query = f"""
-        select rpad(coalesce(EDIPI,""),10," " ) ||
-            rpad(coalesce(Batch_CD,""), 3," ") ||
-            rpad(coalesce(SC_Combined_Disability_Percentage,""),3," ") ||
-            rpad(coalesce(date_format(status_begin_date, 'yyyyMMdd'),""),8," ") ||
-            rpad(coalesce(PT_Indicator,""),1," ") ||
-            rpad(coalesce(Individual_Unemployability,""),1," ") ||  
-            rpad(coalesce(date_format(Status_Last_Update, 'yyyyMMdd'),""),8," ") ||
-            rpad(coalesce(date_format(Status_Termination_Date, 'yyyyMMdd'),""),8," ") as CG
+        select rpad(coalesce(EDIPI,''),10,' ' ) ||
+            rpad(coalesce(Batch_CD,''), 3,' ') ||
+            rpad(coalesce(SC_Combined_Disability_Percentage,''),3,' ') ||
+            rpad(coalesce(date_format(status_begin_date, 'yyyyMMdd'),''),8,' ') ||
+            rpad(coalesce(PT_Indicator,''),1,' ') ||
+            rpad(coalesce(Individual_Unemployability,''),1,' ') ||  
+            rpad(coalesce(date_format(Status_Last_Update, 'yyyyMMdd'),''),8,' ') ||
+            rpad(coalesce(date_format(Status_Termination_Date, 'yyyyMMdd'),''),8,' ') as CG
         from cg_data
         where SDP_Event_Created_Timestamp >= DATE('{last_run_date}')
         and SDP_Event_Created_Timestamp <= DATE('{today_start_time}')
         and EDIPI is not null
-
         and Applicant_Type = 'Primary Caregiver'
         and (Status_Termination_Date is NULL OR Status_Termination_Date >= curdate()
-            OR Status IN ("Approved", "Pending Revocation/Discharge"))
+            OR Status IN ('Approved', 'Pending Revocation/Discharge'))
         and EDIPI is not null
         """
     scd_query = f"""
-        select rpad(coalesce(edipi,""),10," " ) ||
-            rpad(coalesce(Batch_CD,""), 3," ") ||
-            rpad(coalesce(SC_Combined_Disability_Percentage,""),3," ") ||
-            rpad(coalesce(Status_Begin_Date,""),8," ") ||
-            rpad(coalesce(PT_Indicator,""),1," ") ||
-            rpad(coalesce(Individual_Unemployability,""),1," ") ||
-            rpad(coalesce(Status_Last_Update,""),8," ") ||
-            rpad(coalesce(Status_Termination_Date,""),8," ") as CG
+        select rpad(coalesce(edipi,''),10,' ' ) ||
+            rpad(coalesce(Batch_CD,''), 3,' ') ||
+            rpad(coalesce(SC_Combined_Disability_Percentage,''),3,' ') ||
+            rpad(coalesce(Status_Begin_Date,''),8,' ') ||
+            rpad(coalesce(PT_Indicator,''),1,' ') ||
+            rpad(coalesce(Individual_Unemployability,''),1,' ') ||
+            rpad(coalesce(Status_Last_Update,''),8,' ') ||
+            rpad(coalesce(Status_Termination_Date,''),8,' ') as CG
         from scd_data
         where SDP_Event_Created_Timestamp >= DATE('{last_run_date}')
         and SDP_Event_Created_Timestamp <= DATE('{today_start_time}')
@@ -121,12 +96,13 @@ def get_data():
 
 # DBTITLE 1,This function will write the pandas dataframe to a blob storage
 def write_to_patronage(combined_data):
+
     date_format = todays_date.strftime("%Y%m%d")
     outuput_filename = f"PATRONAGE_{date_format}.txt"
     outuput_path = f"/dbfs/mnt/ci-patronage/dmdc_extracts/combined_export/{outuput_filename}"
     pandas_df = spark.sql(combined_data).toPandas()
-
-    if len(pandas_df) == 0:
+    df_record_count = len(pandas_df)
+    if df_record_count == 0:
         dbutils.notebook.exit(f"Notebook exited because there is no data to write")
 
     with open(outuput_path, "w", newline="\r\n") as f:            # DMDC requires output file as unix txt file
@@ -140,15 +116,15 @@ def write_to_patronage(combined_data):
 
     spark.sql(f"""
               INSERT INTO dmdc_checkpoint 
-              (checkpoint_timestamp, filename, record_count) 
-              VALUES ('{today_start_time}','{outuput_path}', {len(pandas_df)})
+              (checkpoint_timestamp, filename, record_count, generate_query)
+              VALUES ('{today_start_time}','{outuput_path}', {df_record_count}, "{combined_data}")
               """
     )
 
     print(f'Data has been written to: {outuput_path}')
-    print(f'Number of records: {len(pandas_df)}')
+    print(f'Number of records: {df_record_count}')
     print(f"Current timestamp: {today_start_time}")
-
+    print(f"Generated Query:\n {combined_data}")
     print(pandas_df.to_string())
 
 # COMMAND ----------
@@ -159,15 +135,22 @@ write_to_patronage(query)
 # COMMAND ----------
 
 # MAGIC %sql
-# MAGIC SELECT * FROM dmdc_checkpoint ORDER BY 1 DESC;
-# MAGIC
+# MAGIC SELECT * FROM dmdc_checkpoint ORDER BY 1 DESC LIMIT 5;
 
 # COMMAND ----------
 
-# %sql
-# delete from dmdc_checkpoint where record_count = 17059
+# query = get_data()
+# print(query)
 
 # COMMAND ----------
 
-query = get_data()
-print(query)
+# from ftplib import FTP
+# ip_add = '140.20.107.4'
+# username = 'patelbn'
+# password = 'w&zuyHqy_XX5zXe4'
+# ftp_directory = '/ftp/data02/pftbat/incoming' 
+# ftp = FTP(ip_add)
+# ftp.login(user=username, passwd=password)
+# ftp.cwd(ftp_directory)
+# with open(outuput_path, "w", newline="\r\n") as f:            # DMDC requires output file as unix txt file
+        # ftp.storbinary(pandas_df.to_string(index=False, header=False))
